6472c1f0fb83cf5c232650d6f6519db290059135
trillobite <jparnell0@protonmail.com>
2018-09-18 00:25:40 -0700

    a simple working api

--- engine/routes/index.js
+++ engine/routes/index.js
@@ -1,20 +1,128 @@
-var express = require('express');
-var router = express.Router();
+const mongo = require("mongodb").MongoClient;
+const mongoURL = "mongodb://178.128.176.202:27017/";
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const router = express.Router();
 
-router.get('/pens', function(req, res){
-  res.send('this is the data for the pens!');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const jsonParser = bodyParser.json();
+
+const getData = (query, collectionName, callbackFunc) => {
+    mongo.connect(mongoURL, (err, db) => {
+        if(err) {
+            callbackFunc(undefined);
+            return; //exit if error.
+        }
+        let dbo = db.db("saberShores");
+        dbo.collection(collectionName).find(query).toArray((err, result) => {
+            if(err) {
+                callbackFunc(undefined);
+            } else {
+                callbackFunc(result);
+            }
+        });
+    });
+};
+
+const postData = (input, collectionName, callbackFunc) => {
+    mongo.connect(mongoURL, (err, db) => {
+        if(err) {
+            callbackFunc(false);
+            return; //exit if error.
+        }
+        let dbo = db.db("saberShores");
+        dbo.collection(collectionName).insertOne(input, (err, result) => {
+            if(err) {
+                callbackFunc(false);
+            } else {
+                callbackFunc(result);
+            }
+        });
+    });
+};
+
+router.get('/pens/:name?', (req, res) => {
+    const name = req.params.name;
+    let query = {};
+    if(name) {
+        query.name = name;
+    }
+    getData(query, "pens", (returned) => {
+        res.send(JSON.stringify(returned));
+    });
 });
 
-router.get('/stamps', function(req, res){
-  res.send('this is the data for the stamps!');
+router.post('/pens', jsonParser, (req, res) => {
+    console.log(req.body);
+    postData(req.body, "pens", (returned) => {
+        res.send(returned ? true : false);
+    });
 });
 
-router.get('/leather', function(req, res){
-  res.send('this is the data for the leather products!');
+router.get('/stamps/:name?', (req, res) => {
+    const name = req.params.name;
+    let query = {};
+    if(name) {
+        query.name = name;
+    }
+    getData(query, "stamps", (returned) => {
+        res.send(JSON.stringify(returned));
+    });
 });
 
-router.get('/jewelry', function(req, res) {
-    res.send('this is the data for the jewelry products!');
+router.post('/stamps', jsonParser, (req, res) => {
+    console.log(req.body);
+    postData(req.body, "stamps", (returned) => {
+        res.send(returned ? true : false);
+    });
+});
+
+router.get('/leather/:name?', (req, res) => {
+    const name = req.params.name;
+    let query = {};
+    if(name) {
+        query.name = name;
+    }
+    getData(query, "leather", (returned) => {
+        res.send(JSON.stringify(returned));
+    });
+});
+
+router.post('/leather', jsonParser, (req, res) => {
+    console.log(req.body);
+    postData(req.body, "leather", (returned) => {
+        res.send(returned ? true : false);
+    });
+});
+
+router.get('/jewelry/:name?', (req, res) => {
+    const name = req.params.name;
+    let query = {};
+    if(name) {
+        query.name = name;
+    }
+    getData(query, "jewelry", (returned) => {
+        res.send(JSON.stringify(returned));
+    });
+});
+
+router.post('/jewelry', jsonParser, (req, res) => {
+    console.log(req.body);
+    postData(req.body, "jewelry", (returned) => {
+        res.send(returned ? true : false);
+    });
+});
+
+app.use("/products", router);
+
+app.listen(3000, () => {
+    console.log("live at port 3000");
 });
 
 module.exports = router;
