eea15e48def59416f50b3f1200cc44c0ca315356
trillobite <jparnell0@protonmail.com>
2018-11-30 22:06:21 -0800

    added security with tokens and roles

--- engine/routes/index.js
+++ engine/routes/index.js
@@ -1,13 +1,17 @@
+const config = require("../config.js");
 const mongo = require("mongodb").MongoClient;
 const mongoURL = "mongodb://178.128.176.202:27017/";
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 const app = express();
 const router = express.Router();
 
 //explicitly allows SAME ORIGIN POLICY ALL.
 app.use(cors());
+
+app.set('superSecret', config.secret); // secret variable
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,16 +21,53 @@ app.use(bodyParser.json());
 
 const jsonParser = bodyParser.json();
 
+const checkMongoID = (query) => {
+    if (query.hasOwnProperty("_id")) {
+        let id = query["_id"];
+        let mongoID = require("mongodb").ObjectId; //creates an object id.
+        query["_id"] = mongoID(id);
+    }
+    return query;
+};
+
+const checkPermission = (role, res, type, callback, collName) => {
+    console.log(role, collName);
+    getData({ role: role }, "roles", (returned) => {
+        console.log(returned);
+        if(returned[0].hasOwnProperty("restricted")) {
+            const permitted = returned[0].restricted.includes(collName);
+            console.log("returned:", returned[0].restricted);
+            console.log("permitted", permitted);
+            if(permitted) {
+                return res.status(403).send({
+                    success: false,
+                    message: "you do not have required permissions!"
+                });
+            }
+        }
+        if (!returned[0].powers.includes(type)) {
+            return res.status(403).send({
+                success: false,
+                message: "you do not have required permissions!"
+            });
+        } else {
+            callback();
+        }
+    });
+};
+
 //gets data from the sabershores mongodb.
 const getData = (query, collectionName, callbackFunc) => {
+    query = checkMongoID(query);
+
     mongo.connect(mongoURL, (err, db) => {
-        if(err) {
+        if (err) {
             callbackFunc(undefined);
             return; //exit if error.
         }
         let dbo = db.db("saberShores");
         dbo.collection(collectionName).find(query).toArray((err, result) => {
-            if(err) {
+            if (err) {
                 callbackFunc(undefined);
             } else {
                 callbackFunc(result);
@@ -39,13 +80,13 @@ const getData = (query, collectionName, callbackFunc) => {
 const postData = (input, collectionName, callbackFunc) => {
     console.log(input);
     mongo.connect(mongoURL, (err, db) => {
-        if(err) {
+        if (err) {
             callbackFunc(false);
             return; //exit if error.
         }
         let dbo = db.db("saberShores");
         dbo.collection(collectionName).insertOne(input, (err, result) => {
-            if(err) {
+            if (err) {
                 callbackFunc(false);
             } else {
                 callbackFunc(result);
@@ -56,14 +97,16 @@ const postData = (input, collectionName, callbackFunc) => {
 
 //deletes data from the sabershores mongodb.
 const deleteData = (query, collectionName, callbackFunc) => {
+    query = checkMongoID(query);
+
     mongo.connect(mongoURL, (err, db) => {
-        if(err) {
+        if (err) {
             callbackFunc(false);
             return; //exit if error.
         }
         let dbo = db.db("saberShores");
         dbo.collection(collectionName).removeOne(query, (err, result) => {
-            if(err) {
+            if (err) {
                 callbackFunc(false);
             } else {
                 callbackFunc(result);
@@ -74,14 +117,23 @@ const deleteData = (query, collectionName, callbackFunc) => {
 
 //updates data to the sabershores mongodb.
 const patchData = (query, update, collectionName, callbackFunc) => {
+    query = checkMongoID(query);
+
+    if (update) { //the update must specify what to set with the $set operator.
+        let set = update;
+        update = {
+            $set: set,
+        };
+    }
     mongo.connect(mongoURL, (err, db) => {
-        if(err) {
+        if (err) {
             callbackFunc(false);
             return; //exit if error.
         }
         let dbo = db.db("saberShores");
+        console.log(query);
         dbo.collection(collectionName).updateOne(query, update, (err, result) => {
-            if(err) {
+            if (err) {
                 callbackFunc(false);
             } else {
                 callbackFunc(result);
@@ -90,328 +142,168 @@ const patchData = (query, update, collectionName, callbackFunc) => {
     });
 };
 
-router.get('/pens/:name?', (req, res) => {
-    const name = req.params.name;
+router.post('/authenticate', jsonParser, (req, res) => {
+    console.log("authenticate!");
+    if (req.body.hasOwnProperty("usrName") && req.body.hasOwnProperty("password")) {
+        getData(req.body, "users", (returned) => {
+            if (returned && returned.length > 0) {
+                console.log(returned);
+                let user = returned[0];
+                const payload = {
+                    role: user.role,
+                };
+                var token = jwt.sign(payload, app.get('superSecret'));
+
+                // return the information including token as JSON
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+
+                //res.send(returned);
+            } else {
+                res.sendStatus(404);
+            }
+        });
+    } else {
+        res.sendStatus(404);
+    }
+
+});
+
+// route middleware to verify a token
+router.use(function (req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded; next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+});
+
+router.get('/:collName?/:option?/:data?', (req, res, next) => {
+    console.log("using generic get");
+    const collName = req.params.collName;
+    const option = req.params.option;
+    const data = req.params.data;
     let query = {};
-    if(name) {
-        query.name = name;
+
+    const executeQuery = () => {
+        if (collName == "collections") {
+            next(); //don't use this function for handling collections...
+            return;
+        }
+        if (option && data) {
+            query[option] = data;
+        }
+        getData(query, collName, (returned) => {
+            if (returned) {
+                res.send(returned);
+            } else {
+                next(); //if it fails, try another...
+                return;
+            }
+        });
     }
-    getData(query, "pens", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
+
+    checkPermission(req.decoded.role, res, "GET", executeQuery, collName);
+
 });
 
-router.post('/pens', jsonParser, (req, res) => {
-    console.log(req.body);
-    postData(req.body, "pens", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
+
+router.post('/:collName?', jsonParser, (req, res) => {
+    console.log("using generic post");
+    const executeQuery = () => {
+        const collName = req.params.collName;
+        if (!collName) {
             res.send(false);
         }
-    });
-});
-
-router.patch('/pens/:id?', jsonParser, (req, res) => {
-    if(!req.params.id) {
-        res.send(false);
+        console.log(req.body);
+        postData(req.body, collName, (returned) => {
+            if (returned) {
+                res.send(returned);
+            } else {
+                res.send(false);
+            }
+        });
     }
-    const id = req.params.id;
-    let query = {
-        "_id": id,
-    };
-    patchData(query, req.body, "pens", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
+
+    checkPermission(req.decoded.role, res, "POST", executeQuery, collName);
 });
 
-router.delete('/pens', jsonParser, (req, res) => {
-    console.log(req.body);
-    deleteData(req.body, "pens", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
+router.patch('/:collName?/:id?', jsonParser, (req, res) => {
+    console.log("using generic patch");
+    const executeQuery = () => {
+        if (!req.params.collName) {
             res.send(false);
         }
-    });
-});
-
-router.get('/stamps/:name?', (req, res) => {
-    const name = req.params.name;
-    let query = {};
-    if(name) {
-        query.name = name;
+        if (!req.params.id) {
+            res.send(false);
+        }
+        const id = req.params.id;
+        const collName = req.params.collName;
+        let query = {
+            "_id": id,
+        };
+        console.log(query);
+        patchData(query, req.body, collName, (returned) => {
+            if (returned) {
+                res.send(returned);
+            } else {
+                res.send(false);
+            }
+        });
     }
-    getData(query, "stamps", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
+
+    checkPermission(req.decoded.role, res, "PATCH", executeQuery, req.params.collName);
 });
 
-router.post('/stamps', jsonParser, (req, res) => {
-    console.log(req.body);
-    postData(req.body, "stamps", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
+router.delete('/:collName?/:id?', (req, res) => {
+    console.log("using generic delete");
+    const executeQuery = () => {
+        if (!req.params.collName) {
             res.send(false);
         }
-    });
-});
-
-router.patch('/stamps/:id?', jsonParser, (req, res) => {
-    if(!req.params.id) {
-        res.send(false);
+        if (!req.params.id) {
+            res.send(false);
+        }
+        const id = req.params.id;
+        const collName = req.params.collName;
+        let query = {
+            "_id": id,
+        };
+        deleteData(query, collName, (returned) => {
+            if (returned) {
+                res.send(returned);
+            } else {
+                res.send(false);
+            }
+        });
     }
-    const id = req.params.id;
-    let query = {
-        "_id": id,
-    };
-    patchData(query, req.body, "stamps", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
 
-router.delete('/stamps', jsonParser, (req, res) => {
-    console.log(req.body);
-    deleteData(req.body, "stamps", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.get('/leather/:name?', (req, res) => {
-    const name = req.params.name;
-    let query = {};
-    if(name) {
-        query.name = name;
-    }
-    getData(query, "leather", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.post('/leather', jsonParser, (req, res) => {
-    console.log(req.body);
-    postData(req.body, "leather", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.patch('/leather/:id?', jsonParser, (req, res) => {
-    if(!req.params.id) {
-        res.send(false);
-    }
-    const id = req.params.id;
-    let query = {
-        "_id": id,
-    };
-    patchData(query, req.body, "leather", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.delete('/leather', jsonParser, (req, res) => {
-    console.log(req.body);
-    deleteData(req.body, "leather", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.get('/jewelry/:name?', (req, res) => {
-    const name = req.params.name;
-    let query = {};
-    if(name) {
-        query.name = name;
-    }
-    getData(query, "jewelry", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.post('/jewelry', jsonParser, (req, res) => {
-    console.log(req.body);
-    postData(req.body, "jewelry", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.patch('/jewelry/:id?', jsonParser, (req, res) => {
-    if(!req.params.id) {
-        res.send(false);
-    }
-    const id = req.params.id;
-    let query = {
-        "_id": id,
-    };
-    patchData(query, req.body, "jewelry", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.delete('/jewelry', jsonParser, (req, res) => {
-    console.log(req.body);
-    deleteData(req.body, "jewelry", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.get('/reviews/:productID?', (req, res) => {
-    const productID = req.params.productID;
-    let query = {};
-    if(productID) {
-        query.productID = productID;
-    }
-    getData(query, "reviews", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.post('/reviews', jsonParser, (req, res) => {
-    console.log(req.body);
-    postData(req.body, "reviews", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.patch('/reviews/:id?', jsonParser, (req, res) => {
-    if(!req.params.id) {
-        res.send(false);
-    }
-    const id = req.params.id;
-    let query = {
-        "_id": id,
-    };
-    patchData(query, req.body, "reviews", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.delete('/reviews', jsonParser, (req, res) => {
-    console.log(req.body);
-    deleteData(req.body, "reviews", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.get('/images/:imageID?', (req, res) => {
-    const imageID = req.params.imageID;
-    let query = {};
-    if(imageID) {
-        query.imageID = imageID;
-    }
-    getData(query, "images", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.post('/images', jsonParser, (req, res) => {
-    console.log(req.body);
-    postData(req.body, "images", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.patch('/images/:id?', jsonParser, (req, res) => {
-    if(!req.params.id) {
-        res.send(false);
-    }
-    const id = req.params.id;
-    let query = {
-        "_id": id,
-    };
-    patchData(query, req.body, "images", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.delete('/images', jsonParser, (req, res) => {
-    console.log(req.body);
-    deleteData(req.body, "images", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
+    checkPermission(req.decoded.role, res, "DELETE", executeQuery, collName);
 });
 
 //Get list of all collections.
@@ -420,49 +312,23 @@ router.get('/collections', (req, res) => {
         res.send(returned);
     };
     mongo.connect(mongoURL, (err, db) => {
-        if(err) {
+        if (err) {
             callbackFunc(undefined);
             return; //exit if error.
         }
         let dbo = db.db("saberShores");
         dbo.collections((err, result) => {
             let names = [];
-            for(var i = 0; i < result.length; ++i) {
+            for (var i = 0; i < result.length; ++i) {
                 names[i] = result[i].s.name;
             }
-            if(err) {
+            if (err) {
                 callbackFunc(undefined);
             } else {
                 console.log(names);
                 callbackFunc(names);
             }
         });
-    });
-});
-
-router.post('/emails', jsonParser, (req, res) => {
-    console.log(req.body);
-    postData(req.body, "emails", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
-    });
-});
-
-router.get('/emails/:emailID?', (req, res) => {
-    const emailID = req.params.imageID;
-    let query = {};
-    if(emailID) {
-        query.emailID = emailID;
-    }
-    getData(query, "emails", (returned) => {
-        if(returned) {
-            res.send(returned);
-        } else {
-            res.send(false);
-        }
     });
 });
 
