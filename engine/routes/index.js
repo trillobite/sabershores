1d5d3bce5b09dbe6f357bf0348a4f12cd0b9de0a
trillobite <jparnell0@protonmail.com>
2018-12-01 19:16:51 -0800

    added async await and promises, and improved security account privleges and access

--- engine/routes/index.js
+++ engine/routes/index.js
@@ -30,142 +30,188 @@ const checkMongoID = (query) => {
     return query;
 };
 
-const checkPermission = (role, res, type, callback, collName) => {
+const checkPermission = async (role, res, type, collName) => {
+    const sendErr = () => { //sends an error if credendials don't match...
+        return res.status(403).send({
+            success: false,
+            message: `Access to ${collName} of type ${type} is not authorized`
+        });
+    }
+
     console.log(role, collName);
-    getData({ role: role }, "roles", (returned) => {
-        console.log(returned);
-        if(returned[0].hasOwnProperty("restricted")) {
-            const permitted = returned[0].restricted.includes(collName);
-            console.log("returned:", returned[0].restricted);
-            console.log("permitted", permitted);
-            if(permitted) {
-                return res.status(403).send({
-                    success: false,
-                    message: "you do not have required permissions!"
-                });
+    let returned = await getData({ role: role }, "roles");
+
+    console.log(returned);
+    if (returned[0].hasOwnProperty("restricted")) {
+
+        const containsEndpoint = (endPnts, find) => {
+            for (let i = 0; i < endPnts.length; ++i) {
+                if (endPnts[i].hasOwnProperty(find)) {
+                    return endPnts[i][find];
+                }
+            }
+            return false;
+        };
+
+        const containsType = (endPoint, type) => {
+            for (let i = 0; i < endPoint.length; ++i) {
+                if (endPoint[i] == type) {
+                    return endPoint[i];
+                }
+            }
+            return false;
+        };
+
+        const restrictedEndpoints = returned[0].restricted;
+        const endPnt = containsEndpoint(restrictedEndpoints, collName);
+        if (endPnt) {
+            if (containsType(endPnt, type)) {
+                return true;
+            } else {
+                return sendErr();
             }
         }
-        if (!returned[0].powers.includes(type)) {
-            return res.status(403).send({
-                success: false,
-                message: "you do not have required permissions!"
-            });
-        } else {
-            callback();
-        }
-    });
+
+    }
+
+    if (!returned[0].powers.includes(type)) {
+        return sendErr();
+    } else { //everything looks good, execute callback!
+        return true;
+    }
 };
 
 //gets data from the sabershores mongodb.
-const getData = (query, collectionName, callbackFunc) => {
-    query = checkMongoID(query);
-
-    mongo.connect(mongoURL, (err, db) => {
-        if (err) {
-            callbackFunc(undefined);
-            return; //exit if error.
+const getData = async (query, collectionName) => {
+    return new Promise((resolve, reject) => {
+        try {
+            query = checkMongoID(query);
+            mongo.connect(mongoURL, (err, db) => {
+                if (err) {
+                    reject(err);
+                }
+                let dbo = db.db("saberShores");
+                dbo.collection(collectionName).find(query).toArray((err, result) => {
+                    console.log("getData", result);
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
+                });
+            });
+        } catch (e) {
+            reject(e);
         }
-        let dbo = db.db("saberShores");
-        dbo.collection(collectionName).find(query).toArray((err, result) => {
-            if (err) {
-                callbackFunc(undefined);
-            } else {
-                callbackFunc(result);
-            }
-        });
     });
 };
 
 //adds data to the sabershores mongodb.
-const postData = (input, collectionName, callbackFunc) => {
-    console.log(input);
-    mongo.connect(mongoURL, (err, db) => {
-        if (err) {
-            callbackFunc(false);
-            return; //exit if error.
+const postData = async (query, collectionName) => {
+    console.log(query);
+    return new Promise((resolve, reject) => {
+        try {
+            query = checkMongoID(query);
+            mongo.connect(mongoURL, (err, db) => {
+                if (err) {
+                    reject(err);
+                }
+                let dbo = db.db("saberShores");
+                dbo.collection(collectionName).insertOne(query, (err, result) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
+                });
+            });
+        } catch (e) {
+            reject(e);
         }
-        let dbo = db.db("saberShores");
-        dbo.collection(collectionName).insertOne(input, (err, result) => {
-            if (err) {
-                callbackFunc(false);
-            } else {
-                callbackFunc(result);
-            }
-        });
     });
 };
 
 //deletes data from the sabershores mongodb.
-const deleteData = (query, collectionName, callbackFunc) => {
-    query = checkMongoID(query);
-
-    mongo.connect(mongoURL, (err, db) => {
-        if (err) {
-            callbackFunc(false);
-            return; //exit if error.
+const deleteData = async (query, collectionName) => {
+    return new Promise((resolve, reject) => {
+        try {
+            query = checkMongoID(query);
+            mongo.connect(mongoURL, (err, db) => {
+                if (err) {
+                    reject(err);
+                }
+                let dbo = db.db("saberShores");
+                dbo.collection(collectionName).removeOne(query, (err, result) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
+                });
+            });
+        } catch (e) {
+            reject(e);
         }
-        let dbo = db.db("saberShores");
-        dbo.collection(collectionName).removeOne(query, (err, result) => {
-            if (err) {
-                callbackFunc(false);
-            } else {
-                callbackFunc(result);
-            }
-        });
     });
 };
 
 //updates data to the sabershores mongodb.
-const patchData = (query, update, collectionName, callbackFunc) => {
-    query = checkMongoID(query);
+const patchData = async (query, update, collectionName) => {
+    return new Promise((resolve, reject) => {
+        try {
+            query = checkMongoID(query);
 
-    if (update) { //the update must specify what to set with the $set operator.
-        let set = update;
-        update = {
-            $set: set,
-        };
-    }
-    mongo.connect(mongoURL, (err, db) => {
-        if (err) {
-            callbackFunc(false);
-            return; //exit if error.
-        }
-        let dbo = db.db("saberShores");
-        console.log(query);
-        dbo.collection(collectionName).updateOne(query, update, (err, result) => {
-            if (err) {
-                callbackFunc(false);
-            } else {
-                callbackFunc(result);
+            if (update) { //the update must specify what to set with the $set operator.
+                let set = update;
+                update = {
+                    $set: set,
+                };
             }
-        });
+
+            mongo.connect(mongoURL, (err, db) => {
+                if (err) {
+                    reject(err);
+                }
+                let dbo = db.db("saberShores");
+                dbo.collection(collectionName).updateOne(query, update, (err, result) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
+                });
+            });
+        } catch (e) {
+            reject(e);
+        }
     });
 };
 
-router.post('/authenticate', jsonParser, (req, res) => {
+router.post('/authenticate', jsonParser, async (req, res) => {
     console.log("authenticate!");
     if (req.body.hasOwnProperty("usrName") && req.body.hasOwnProperty("password")) {
-        getData(req.body, "users", (returned) => {
-            if (returned && returned.length > 0) {
-                console.log(returned);
-                let user = returned[0];
-                const payload = {
-                    role: user.role,
-                };
-                var token = jwt.sign(payload, app.get('superSecret'));
 
-                // return the information including token as JSON
-                res.json({
-                    success: true,
-                    message: 'Enjoy your token!',
-                    token: token
-                });
+        let returned = await getData(req.body, "users");
 
-                //res.send(returned);
-            } else {
-                res.sendStatus(404);
-            }
-        });
+        if (returned && returned.length > 0) {
+            console.log(returned);
+            let user = returned[0];
+            const payload = {
+                role: user.role,
+            };
+            var token = jwt.sign(payload, app.get('superSecret'));
+
+            // return the information including token as JSON
+            res.json({
+                success: true,
+                message: 'Enjoy your token!',
+                token: token
+            });
+
+            //res.send(returned);
+        } else {
+            res.sendStatus(404);
+        }
     } else {
         res.sendStatus(404);
     }
@@ -203,107 +249,134 @@ router.use(function (req, res, next) {
     }
 });
 
-router.get('/:collName?/:option?/:data?', (req, res, next) => {
+router.get('/:collName?/:option?/:data?', async (req, res, next) => {
     console.log("using generic get");
     const collName = req.params.collName;
     const option = req.params.option;
     const data = req.params.data;
     let query = {};
 
-    const executeQuery = () => {
-        if (collName == "collections") {
-            next(); //don't use this function for handling collections...
-            return;
-        }
-        if (option && data) {
-            query[option] = data;
-        }
-        getData(query, collName, (returned) => {
-            if (returned) {
-                res.send(returned);
-            } else {
-                next(); //if it fails, try another...
+    try {
+        const permission = await checkPermission(req.decoded.role, res, "GET", collName);
+        console.log("permission:", permission);
+        if (permission) {
+            if (collName == "collections") {
+                next(); //don't use this function for handling collections...
                 return;
             }
+            if (option && data) {
+                query[option] = data;
+            }
+            let returned = await getData(query, collName);
+            if (returned) {
+                res.send(returned);
+            } else {
+                next();
+                return;
+            }
+        }
+    } catch (e) {
+        return res.status(500).send({
+            success: false,
+            message: `Internal server error ${e}`
         });
     }
-
-    checkPermission(req.decoded.role, res, "GET", executeQuery, collName);
-
 });
 
 
-router.post('/:collName?', jsonParser, (req, res) => {
+router.post('/:collName?', jsonParser, async (req, res) => {
     console.log("using generic post");
-    const executeQuery = () => {
-        const collName = req.params.collName;
-        if (!collName) {
-            res.send(false);
-        }
-        console.log(req.body);
-        postData(req.body, collName, (returned) => {
+    const collName = req.params.collName;
+
+    try {
+        const permission = await checkPermission(req.decoded.role, res, "POST", collName);
+        console.log("permission:", permission);
+        if (permission) {
+
+            if (!collName) {
+                res.send(false);
+            }
+            console.log(req.body);
+            const returned = await postData(req.body, collName);
             if (returned) {
                 res.send(returned);
             } else {
                 res.send(false);
             }
+        }
+    } catch (e) {
+        return res.status(500).send({
+            success: false,
+            message: `Internal server error ${e}`
         });
     }
-
-    checkPermission(req.decoded.role, res, "POST", executeQuery, collName);
 });
 
-router.patch('/:collName?/:id?', jsonParser, (req, res) => {
+router.patch('/:collName?/:id?', jsonParser, async (req, res) => {
     console.log("using generic patch");
-    const executeQuery = () => {
-        if (!req.params.collName) {
-            res.send(false);
-        }
-        if (!req.params.id) {
-            res.send(false);
-        }
-        const id = req.params.id;
-        const collName = req.params.collName;
-        let query = {
-            "_id": id,
-        };
-        console.log(query);
-        patchData(query, req.body, collName, (returned) => {
+
+    try {
+        const permission = await checkPermission(req.decoded.role, res, "PATCH", req.params.collName);
+        console.log(permission);
+        if (permission) {
+            if (!req.params.collName) {
+                res.send(false);
+            }
+            if (!req.params.id) {
+                res.send(false);
+            }
+            const id = req.params.id;
+            const collName = req.params.collName;
+            let query = {
+                "_id": id,
+            };
+            console.log(query);
+            const returned = await patchData(query, req.body, collName);
             if (returned) {
                 res.send(returned);
             } else {
                 res.send(false);
             }
+        }
+    } catch (e) {
+        return res.status(500).send({
+            success: false,
+            message: `Internal server error ${e}`
         });
     }
-
-    checkPermission(req.decoded.role, res, "PATCH", executeQuery, req.params.collName);
 });
 
-router.delete('/:collName?/:id?', (req, res) => {
+router.delete('/:collName?/:id?', async (req, res) => {
     console.log("using generic delete");
-    const executeQuery = () => {
-        if (!req.params.collName) {
-            res.send(false);
-        }
-        if (!req.params.id) {
-            res.send(false);
-        }
-        const id = req.params.id;
-        const collName = req.params.collName;
-        let query = {
-            "_id": id,
-        };
-        deleteData(query, collName, (returned) => {
+    const collName = req.params.collName;
+    try {
+        const permission = await checkPermission(req.decoded.role, res, "DELETE", collName);
+        console.log(permission);
+        if (permission) {
+            if (!req.params.collName) {
+                res.send(false);
+            }
+            if (!req.params.id) {
+                res.send(false);
+            }
+            const id = req.params.id;
+            let query = {
+                "_id": id,
+            };
+
+            const returned = await deleteData(query, collName);
             if (returned) {
                 res.send(returned);
             } else {
                 res.send(false);
             }
+        }
+    } catch (e) {
+        return res.status(500).send({
+            success: false,
+            message: `Internal server error ${e}`
         });
     }
-
-    checkPermission(req.decoded.role, res, "DELETE", executeQuery, collName);
 });
 
 //Get list of all collections.
